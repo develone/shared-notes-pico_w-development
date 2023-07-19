240e37641bcdfd41a49f2adf1de2cae39c85c783
Ed Vidal Jr <develone@sbcglobal.net>
2023-07-19 05:47:08 -0600

    adding rp2040-freertos-project

--- update-repos.sh
+++ update-repos.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~/pico_w-mqtt/
- git pull
- cd ~/shared-notes-pico_w-development/
- git pull
- ~/my-projects-docs/
- git pull
+git pull
+cd ~/shared-notes-pico_w-development/
+git pull
+cd  ~/my-projects-docs/
+git pull
+cd ~/rp2040-freertos-project
+git pull
 cd ~/
